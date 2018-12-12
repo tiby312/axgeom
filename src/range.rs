306aec16e0693d91ea6a1f6d5ff038a6482dad97
Ken Reed <kenakioreed@gmail.com>
2018-12-11 21:15:49 -0500

    new version

--- src/range.rs
+++ src/range.rs
@@ -4,7 +4,7 @@ use std;
 ///This means that subdivision does not result in any floating point calculations.
 ///The left value be <= the right value.
 ///There is no protection against "degenerate" Ranges where left>right.
-///Unlike std::ops::Range, It is a fully closed range. So [], not [) or ().
+///Unlike std::ops::Range, It is a fully closed range. Points exactly on the borders are considered inside the range.
 
 
 #[derive(Copy,Clone,Debug,Eq,PartialEq)]
@@ -16,22 +16,21 @@ pub struct Range<T:Copy>{
 impl<T:Copy+PartialOrd> Range<T>{
 
     ///Returns true if the point is inside of the range or on top of.
-    ///True if pos>=self.left&&pos<=self.right 
-    #[inline(always)]
+    #[inline]
     pub fn contains(&self, pos: T) -> bool {
         pos>=self.left&&pos<=self.right
     }
 }
 
 impl<T:Copy+std::ops::Sub<Output=T>> Range<T>{
-    #[inline(always)]
+    #[inline]
     pub fn len(&self)->T{
         self.right-self.left
     }
 }
 
 impl<T:Copy+std::ops::Sub<Output=T>+std::ops::Add<Output=T>> Range<T>{
-    #[inline(always)]
+    #[inline]
     pub fn grow(&mut self,radius:T)->&mut Self{
         self.right=self.right+radius;
         self.left=self.left-radius;
@@ -43,7 +42,7 @@ impl<T:Copy+Ord> Range<T>{
     ///If the pos is to the left of the range, return less.
     ///If the pos is to the right of the range, return greater.
     ///If the pos intersects with the range, return equal.
-    #[inline(always)]
+    #[inline]
     pub fn left_or_right_or_contain(&self,pos:&T)->std::cmp::Ordering{
         
         if *pos<self.left{
@@ -55,7 +54,7 @@ impl<T:Copy+Ord> Range<T>{
         }
     }
 
-    #[inline(always)]
+    #[inline]
     pub fn grow_to_fit(&mut self,b:&Range<T>){
         
         let a=self;  
@@ -70,13 +69,13 @@ impl<T:Copy+Ord> Range<T>{
 
 
     ///Returns true if self contains the specified range.
-    #[inline(always)]
+    #[inline]
     pub fn contains_range(&self, val: &Range<T>) -> bool {
         self.contains(val.left) && self.contains(val.right)
     }
 
     ///Creates a range that represents the intersection range.
-    #[inline(always)]
+    #[inline]
     pub fn get_intersection(&self,val:&Range<T>)->Option<Range<T>>{
   
         let a=self.left.max(val.left);
@@ -89,7 +88,7 @@ impl<T:Copy+Ord> Range<T>{
     }
 
     ///Returns true if two ranges intersect.
-    #[inline(always)]
+    #[inline]
     pub fn intersects(&self, val: &Range<T>) -> bool {
         //TODO double check this?
         self.contains(val.left) || val.contains(self.left)
--- src/rect.rs
+++ src/rect.rs
@@ -4,7 +4,7 @@ use range::Range;
 use *;
 
 ///An axis aligned rectangle. Stored as two Ranges. 
-///It is a fully closed rectangle. So [], not [) or ().
+///It is a fully closed rectangle. Points exactly along the border of the rectangle are considered inside the rectangle. 
 #[repr(transparent)]
 #[derive(Copy,Clone,Debug)]
 #[must_use]
@@ -17,7 +17,7 @@ impl<T:Copy> Rect<T>{
 
     ///(a,b) is the x component range.
     ///(c,d) is the y component range.
-    #[inline(always)]
+    #[inline]
     pub fn new(a:T,b:T,c:T,d:T)->Rect<T>{
         let r1=Range{left:a,right:b};
         let r2=Range{left:c,right:d};
@@ -26,13 +26,13 @@ impl<T:Copy> Rect<T>{
 
     ///(a,b) is the x component range.
     ///(c,d) is the y component range.
-    #[inline(always)]
+    #[inline]
     pub fn get(&self)->((T,T),(T,T)){
         let f=&self.0;
         ((f[0].left,f[0].right),(f[1].left,f[1].right))
     }
 
-    #[inline(always)]
+    #[inline]
     pub fn get_range(&self,axis:impl AxisTrait)->&Range<T>{
         if axis.is_xaxis(){
             &self.0[0]
@@ -40,7 +40,7 @@ impl<T:Copy> Rect<T>{
             &self.0[1]
         }
     }
-    #[inline(always)]
+    #[inline]
     pub fn get_range_mut(&mut self,axis:impl AxisTrait)->&mut Range<T>{
         if axis.is_xaxis(){
             &mut self.0[0]
@@ -52,7 +52,7 @@ impl<T:Copy> Rect<T>{
 
 impl<T:PartialOrd+Copy> Rect<T>{
 
-    #[inline(always)]
+    #[inline]
     pub fn contains_point(&self,a:[T;2])->bool{
         self.get_range(XAXISS).contains(a[0]) &&
         self.get_range(YAXISS).contains(a[1])
@@ -61,7 +61,7 @@ impl<T:PartialOrd+Copy> Rect<T>{
 
 
 impl<T:Copy+std::ops::Sub<Output=T>+std::ops::Add<Output=T>> Rect<T>{
-    #[inline(always)]
+    #[inline]
     pub fn grow(&mut self,radius:T)->&mut Self{
         self.0[0].grow(radius);
         self.0[1].grow(radius);
@@ -70,7 +70,7 @@ impl<T:Copy+std::ops::Sub<Output=T>+std::ops::Add<Output=T>> Rect<T>{
 }
 
 impl<T:Ord+Copy> Rect<T>{
-    #[inline(always)]
+    #[inline]
     pub fn equals(&self,a:&Rect<T>)->bool{
         //TODO optimize
         let ((a1,b1),(c1,d1))=self.get();
@@ -81,7 +81,7 @@ impl<T:Ord+Copy> Rect<T>{
     
     ///Subdivides the rectangle.
     ///No floating point calculations are done (so no precision loss/rounding issues).
-    #[inline(always)]
+    #[inline]
     pub fn subdivide<A:AxisTrait>(&self, axis:A,mut divider: T) -> (Rect<T>,Rect<T>) {
         
         let ca=axis;
@@ -114,7 +114,7 @@ impl<T:Ord+Copy> Rect<T>{
 
 
     ///Returns true if the specified rect is inside of this rect.
-    #[inline(always)]
+    #[inline]
     pub fn contains_rect(&self,rect:&Rect<T>)->bool{
 
         //This seems like something a macro would be suited for.
@@ -132,7 +132,7 @@ impl<T:Ord+Copy> Rect<T>{
     
     ///Grow the rectangle to fit the specified rectangle by replacing values
     ///with the specified rectangle. No floating point computations.
-    #[inline(always)]
+    #[inline]
     pub fn grow_to_fit(&mut self,rect:&Rect<T>)->&mut Self{
         {
             macro_rules! macro_axis
@@ -143,14 +143,6 @@ impl<T:Ord+Copy> Rect<T>{
                         let sx=self.get_range_mut($axis);
                         let rx=rect.get_range($axis);
                         sx.grow_to_fit(rx);
-                        /*
-                        if rx.left<sx.left{
-                            sx.left=rx.left;
-                        }
-                        if rx.right>sx.right{
-                            sx.right=rx.right;
-                        }
-                        */
                     }
                 }
             }
@@ -161,7 +153,7 @@ impl<T:Ord+Copy> Rect<T>{
         self
     }
     
-    #[inline(always)]
+    #[inline]
     pub fn intersects_rect(&self,other:&Rect<T>)->bool{
         macro_rules! macro_axis{
             ($axis:ident)=>{
@@ -184,7 +176,7 @@ impl<T:Ord+Copy> Rect<T>{
     ///Get an intersecting rectangle.
     ///No floating point calculations as the new rectangle is made up of
     ///values from this rectangle and the specified rectangle.
-    #[inline(always)]
+    #[inline]
     pub fn get_intersect_rect(&self,other:&Rect<T>)->Option<Rect<T>>{
         
         macro_rules! macro_axis{
